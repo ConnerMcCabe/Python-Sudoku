5e3e58b1402b59c64d083818f8ca290dc8061f19
Conner McCabe <conner.p.mccabe@gmail.com>
2021-01-21 13:54:10 -0600

    printing board in console

--- solver.py
+++ solver.py
@@ -1,7 +1,5 @@
 
-
-
-
+# possible game board
 board = [
     [6, 0, 0, 3, 0, 4, 0, 0, 0],
     [9, 8, 0, 0, 5, 0, 0, 0, 0],
@@ -13,3 +11,19 @@ board = [
     [7, 0, 8, 0, 0, 2, 3, 0, 5],
     [0, 5, 0, 0, 0, 0, 2, 0, 0],
 ]
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - -")
+        
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+print_board(board)
